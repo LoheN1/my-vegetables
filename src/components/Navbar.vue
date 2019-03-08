821580e777f8b0178b6fa1605882c95e24af5919
Nicolas Lohe <nicolas.lohe@niji.fr>
2019-03-08 15:21:08 +0100

    init project

---
+++ src/components/Navbar.vue
@@ -0,0 +1,34 @@
+<template>
+  <nav>
+    <v-toolbar dark color="green lighten-1" app>
+      <v-toolbar-side-icon @click="drawer = !drawer"></v-toolbar-side-icon>
+      <v-toolbar-title class="headline text-uppercase">
+        <span>My Vegetables</span>
+      </v-toolbar-title>
+      <v-spacer></v-spacer>
+      <v-btn icon>
+        <v-icon>search</v-icon>
+      </v-btn>
+      <v-btn icon>
+        <v-icon>view_module</v-icon>
+      </v-btn>
+      <v-btn icon>
+        <v-icon>view_list</v-icon>
+      </v-btn>
+    </v-toolbar>
+
+    <v-navigation-drawer v-model="drawer" app color="green darken-3">
+      <p>test</p>
+    </v-navigation-drawer>
+  </nav>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      drawer: false
+    };
+  }
+};
+</script>
---
+++ src/views/Home.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="home">
+    <h1>Hello</h1>
+    <div>
+      <v-btn depressed small>Normal</v-btn>
+    </div>
+    <div>
+      <v-btn depressed small color="secondary">Primary</v-btn>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
